12fc43c2e0a7e8d28608dc255eec9c0f028dda75
bestpickles <kowalczyk.ep@gmail.com>
2024-10-18 09:27:14 +0000

    main file

---
+++ Łajka/main.tf
@@ -0,0 +1,10 @@
+
+
+
+
+# Create a resource group
+resource "azurerm_resource_group" "crew_members_via_tf" {
+  name     = "crew_members_via_tf"
+  location = "West Europe"
+}
+
--- Łajka/provider.tf
+++ Łajka/provider.tf
@@ -12,4 +12,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-}
+} 
