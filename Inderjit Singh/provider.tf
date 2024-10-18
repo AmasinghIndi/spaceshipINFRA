6ad0a3c96dce09a4d5b7102f247acb4fb09bbcfe
AmasinghIndi <148063603+AmasinghIndi@users.noreply.github.com>
2024-10-18 10:02:11 +0000

    new changes

--- Inderjit Singh/provider.tf
+++ Inderjit Singh/provider.tf
@@ -20,4 +20,20 @@ resource "azurerm_resource_group" "example" {
   tags = {
     owner = "inderjit.singh@redbull.com"
   }
-}#
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "indi-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "indi-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+#
