469ffadaa2ca4c44e4323f54cb7552523d78ff74
bestpickles <kowalczyk.ep@gmail.com>
2024-10-18 09:34:30 +0000

    tag updated

--- Łajka/main.tf
+++ Łajka/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "crew_members_via_tf" {
   name     = "crew_members_via_tf"
   location = "West Europe"
   tags = {
-    owner = "Łajka"
+    owner = "kacper.kowalczyk@redbull.com"
   }
 }
 
