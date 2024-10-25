6e50859873659c2a010009c4a668aac9fbb05478
MOCApplication <Johann.Lechner86@gmail.com>
2024-10-25 11:20:28 +0000

    Hans Multi Regional

--- Central/Hans.tf
+++ Central/Hans.tf
@@ -37,3 +37,45 @@ module "hans-vnet" {
   subnet_name         = "hans-subnet"
   subnet_prefixes     = ["11.0.1.0/24"]
 }
+
+//--------------------
+
+# Create a resource group
+resource "azurerm_resource_group" "hans-rg-us-us" {
+  name     = "hans-rg-us"
+  location = "East US"
+  tags = {
+    owner = "Franz.Martinek@redbull.com"
+  }
+}
+
+module "hans-vm-us" {
+  source                        = "./modules/vm"
+  nic_name                      = "hans-nic"
+  location                      = azurerm_resource_group.hans-rg-us.location
+  resource_group_name           = azurerm_resource_group.hans-rg-us.name
+  //subnet_id                   = azurerm_subnet.hans-subnet.id
+  # when using vnet module:
+  subnet_id                     = module.hans-vnet-us.subnet_id
+  vm_name                       = "hansVM"
+  vm_size                       = "Standard_DS1_v2"
+  os_disk_name                  = "example-os-disk"
+  image_publisher               = "Canonical"
+  image_offer                   = "UbuntuServer"
+  image_sku                     = "18.04-LTS"
+  image_version                 = "latest"
+  computer_name                 = "hostname"
+  admin_username                = "adminuser"
+  admin_password                = "Password1234!"
+  disable_password_authentication = false
+}
+
+module "hans-vnet-us" {
+  source              = "./modules/vnet"
+  vnet_name           = "hans-vnet-us"
+  address_space       = ["11.0.0.0/16"]
+  location            = azurerm_resource_group.hans-rg-us.location
+  resource_group_name = azurerm_resource_group.hans-rg-us.name
+  subnet_name         = "hans-subnet"
+  subnet_prefixes     = ["11.0.1.0/24"]
+}
