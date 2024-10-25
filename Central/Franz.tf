e99898f09afa3c5adcc098147c7c8cf7e71f3255
AmasinghIndi <148063603+AmasinghIndi@users.noreply.github.com>
2024-10-25 08:36:25 +0000

    changes in tf files

--- Central/Franz.tf
+++ Central/Franz.tf
@@ -1,5 +1,5 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "franz" {
   name     = "rg-FRMA"
   location = "West Europe"
   tags = {
@@ -7,43 +7,43 @@ resource "azurerm_resource_group" "example" {
   }
 }
  
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "franz" {
   name                = "FRMA-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.franz.location
+  resource_group_name = azurerm_resource_group.franz.name
 }
  
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "franz" {
   name                 = "FRMA-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.franz.name
+  virtual_network_name = azurerm_virtual_network.franz.name
   address_prefixes     = ["10.0.1.0/24"]
  
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "franz" {
   name                = "FRMA-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.franz.location
+  resource_group_name = azurerm_resource_group.franz.name
  
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.franz.id
     private_ip_address_allocation = "Dynamic"
   }
 }
  
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "franz" {
   name                = "FRMA-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.franz.name
+  location            = azurerm_resource_group.franz.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password = "testFRMA15"
   disable_password_authentication = "false"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.franz.id,
   ]
  
   os_disk {
--- Central/Hans.tf
+++ Central/Hans.tf
@@ -1,5 +1,5 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "hans" {
   name     = "jlk_resource_group"
   location = "West Europe"
 
@@ -8,37 +8,37 @@ tags = {
   }
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "hans" {
   name                = "jlk_vnet"
   address_space       = ["10.0.0.0/16"]
   location            = "West Europe"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.hans.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "hans" {
   name                 = "jlk_subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.hans.name
+  virtual_network_name = azurerm_virtual_network.hans.name
   address_prefixes     = ["10.0.1.0/24"]
 
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_public_ip" "hans" {
+  name                = "hans-pip"
+  location            = azurerm_resource_group.hans.location
+  resource_group_name = azurerm_resource_group.hans.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "hans" {
   name                = "windowsvm_jlk_nic"
   location            = "West Europe"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.hans.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.hans.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.hans.id
   }
 }
